a99680f6328f2c2630c7858562df93fade9c4328
Carlos Paparoni <carlos.paparoni@gmail.com>
2022-05-02 19:51:33 -0400

    Add initial models and migration

--- engines/models.py
+++ engines/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Engine(models.Model):
+    name = models.CharField(max_length=200)
+    manufacturer = models.CharField(max_length=200)
+    thrust_newtons_sea_level = models.DecimalField(max_digits=19, decimal_places=2, default=0.00)
+    thrust_newtons_vacuum = models.DecimalField(max_digits=19, decimal_places=2, default=0.00)
+    specific_impulse_sea_level = models.IntegerField(default=0)
+    specific_impulse_vacuum = models.IntegerField(default=0)
+    image_url = models.URLField(max_length=200)
+
+
+class Rocket(models.Model):
+    engines = models.ManyToManyField(Engine, through='RocketEngine')
+    name = models.CharField(max_length=200)
+    manufacturer = models.CharField(max_length=200)
+    image_url = models.URLField(max_length=200)
+
+
+class RocketEngine(models.Model):
+    rocket = models.ForeignKey(Rocket, on_delete=models.CASCADE)
+    engine = models.ForeignKey(Engine, on_delete=models.CASCADE)
+    amount = models.IntegerField(default=0)
+    stage  = models.IntegerField(default=0)
